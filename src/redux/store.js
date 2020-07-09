ce51a8367c02792628f3ccb77e8a7ec8841cf90a
abhinavsri360 <abhinav.022000@gmail.com>
2020-07-09 19:50:38 +0530

    avoid logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { Auth } from './auth'
 import thunk from 'redux-thunk'
-import logger from 'redux-logger'
+//import logger from 'redux-logger'
 
 export const ConfigureStore = () => {
   const store = createStore(
     combineReducers({
       auth: Auth
     }),
-    applyMiddleware(thunk, logger)
+    applyMiddleware(thunk)
+    // applyMiddleware(thunk, logger)
   )
   return store
 }
