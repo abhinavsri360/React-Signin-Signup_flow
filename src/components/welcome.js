412cd30066d5bcc6291b1fffc40d3c3df937ac54
abhinavsri360 <abhinav.022000@gmail.com>
2020-07-10 18:57:03 +0530

    welcome wood

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -5,19 +5,15 @@ class home extends Component {
   render () {
     if (this.props.isAuthenticated) {
       return (
-        <body className='body'>
-          <div className='welcome'>
-            <h1 className='neon'>Welcome! {this.props.user.username}</h1>
-          </div>
-        </body>
+        <div className='welcome'>
+          <h1 className='neon'>Welcome! {this.props.user.username}</h1>
+        </div>
       )
     } else {
       return (
-        <body className='body'>
-          <div className='welcome'>
-            <h1 className='neon'>Welcome!</h1>
-          </div>
-        </body>
+        <div className='welcome'>
+          <h1 className='neon'>Welcome!</h1>
+        </div>
       )
     }
   }
